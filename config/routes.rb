b2aa8a826e64ebce3bb11d572653d310c7d9c306
Anne-Marie Nault <anne.ma@gmail.com>
2021-05-13 16:36:24 -0400

    added more styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
-  delete "/bookmarks/:id", to: "bookmarks#destroy"
+  delete "/bookmarks/:id", to: "bookmarks#destroy", as: :bookmark
 end
