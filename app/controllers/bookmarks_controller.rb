68580d6884bbb61195b3642fc8c32bafd92efa43
Anne-Marie Nault <anne.ma@gmail.com>
2021-05-13 13:48:06 -0400

    add lists routes, controller and views; add bookmarks routes and controller

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,7 @@
+class BookmarksController < ApplicationController
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+end
