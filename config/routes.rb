4c6aadcf05ddb2d238045b41477ee668cc318e66
Anne-Marie Nault <anne.ma@gmail.com>
2021-05-14 15:30:01 -0400

    added reviews to lists

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "lists#index"
   resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:create]
+    resources :reviews, only: [:create]
   end
   delete "/bookmarks/:id", to: "bookmarks#destroy", as: :bookmark
 end
